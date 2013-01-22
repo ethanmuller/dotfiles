2ec255a885f7aa0d45cee353c235eb9c68890726
ethanmuller <ethanbmuller@gmail.com>
2013-01-21 22:10:11 -0500

    Make git aliases better

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,6 +1,7 @@
 #[MISC]
 alias c=clear
 alias h=history
+alias refresh='source ~/.bashrc && clear'
 alias l='ls -Gl'
 alias cdg='cd ~/github'
 alias mopen='open http://localhost:8888'
@@ -8,19 +9,17 @@ alias mopen='open http://localhost:8888'
 #[GIT]
 alias git=/usr/local/git/bin/git
 alias g='cat ~/.gitconfig'
-alias gs='git s' # pretty git status
-alias ga='git add '
-alias gb='git branch '
+alias gpu='git push'
+alias gpl='git pull'
+alias gs='git status -sb'
+alias ga='git add'
+alias gb='git branch'
 alias gc='git commit'
 alias gd='git diff'
-alias go='git checkout '
-alias gl='git log --format='\''%Cgreen%h%Creset %C(cyan)%an%Creset - %s'\'' --graph'
-alias glog='git log --format='\''%Cgreen%h%Creset %C(cyan)%an%Creset - %s - %C(yellow)%ar'\'' --graph'
-alias gk='gitk --all&'
-alias gx='gitx --all'
-alias gpb='git publish-branch'
-alias got='git '
-alias get='git '
+alias gsh='git show'
+alias gk='git checkout'
+alias gl='git log'
+#alias gl='git log --format="%Cgreen%h%Creset %C(cyan)%an%Creset - %s %C(yellow)- %ar" --graph'
 
 #[RoR]
 alias ror='cd ~/dev/_rails'

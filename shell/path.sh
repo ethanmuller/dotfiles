b268281463d826a711057c4c8bca4fa154f99741
ethanmuller <ethanbmuller@gmail.com>
2014-10-12 23:07:32 -0500

    clean cruft

--- shell/path.sh
+++ shell/path.sh
@@ -1,6 +1,7 @@
-# Add on to your PATH here
-# Used by both zsh and bash
-PATH="~/bin:$PATH" # for to add personal files to PATH
-PATH="/usr/local/bin:$PATH" # add homebrew-installed packages to path
-PATH="~/.rvm/bin:$PATH" # Add RVM to PATH for scripting
+# For homebrew
+PATH="/usr/local/bin:$PATH"
+
+# For RVM
+PATH="$PATH:$HOME/.rvm/bin"
+
 export PATH
